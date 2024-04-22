9d4009d013c83d6900f6bc38997f944b95a87d2e
isabel-moreno22 <isabel.moreno@genstudents.org>
2024-04-22 17:11:13 -0500

    html y css

---
+++ javascript.js
@@ -0,0 +1,9 @@
+let edad = 23;
+
+if (edad >= 18) {
+    if (edad > 20){
+        return console.log("Tiene más de 20")
+    }
+    return console.log("Es mayor de edad")
+}
+console.log("No es mayor de edad")
